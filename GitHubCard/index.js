c7d458adc8ad0912b7f230f58cc946a65179c6b5
Krishan Nagin <krishannagin@krishans-air.lan>
2021-09-30 15:28:51 -0700

    finished assignment

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,70 +1,52 @@
-const { default: axios } = require("axios");
-
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
-
 */
 
-  //create a cards variable in JSS from HTML
-  const cards = document.querySelector('.cards');
+import axios from 'axios'
+const startPaste = document.querySelector('.cards') // select the startingpoint where you will add cards
 
-
-axios.get('https://api.github.com/users/nagoadvisory')
+//create a function called pullInfo that pulls from axios database and inputs that data into the function cardmaker. when created it will appear below the starting point denoted above
+const pullInfo = (username) => {
+  axios.get(`https://api.github.com/users/${username}`)
   .then(resp =>{
-    console.log(resp);
+    startPaste.appendChild(cardMaker(resp.data))
   })
-  .catch(resp =>{
-    console.error(err);
-  })
-  .finally(resp =>{
-    console.log('IDGAF');
-  })
+  // .then(({data})=> {
+  //   startPaste.appendChild(cardMaker(data)) this could have worked as well
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
-
-    Skip to STEP 3 (line 34).
-*/
-
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
-
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
+   .catch(err => console.log(err))
+}
 
 const followersArray = [
+  'nagoadvisory',
   'tetondan',
   'dustinmyers',
   'justsml',
   'luishrd',
   'bigknell'
-]
+]; //these are the sample followers in the array
 
-followersArray.forEach(object => {
-  axios.get('https:api.github.com/users/${object}').then(res => {
-    cardsDiv.appendChild(cardBuilder(res.data))
-  })
-})
+
+// STEP 4: Pass the data received from Github into your function, and append the returned markup to the DOM as a child of .cards
+followersArray.forEach(username => {
+  pullInfo(username)
+}) // this says that for each ID in the array (aka USERNAME), plug that username into the pullInfo function 
+
+/*
+  STEP 2: Inspect and study the data coming back, this is YOUR
+    github info! You will need to understand the structure of this
+    data in order to use it to build your component function
+    Skip to STEP 3.
+*/
+
+/* STEP 5: Now that you have your own card getting added to the DOM, either follow this link in your browser https://api.github.com/users/<Your github name>/followers, manually find some other users' github handles, or use the list found at the bottom of the page. Get at least 5 different Github usernames and add them a Individual strings to the friendsArray below. Using that array, iterate over it, requesting data for each user, creating a new card for each user, and adding that card to the DOM.
+*/
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -80,63 +62,61 @@ followersArray.forEach(object => {
       </div>
     </div>
 */
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
 
-function cardBuilder ({avatar_url, name, login, location, html_url, followers, following, bio}) {
-//first step is to build JSS variables from element structure
-  const cardCard = document.createElement('div');
-  const cardImage = document.createElement('img');
-  const cardInfoDiv = document.createElement('div');
-  const cardName = document.createElement('h3');
-  const cardUsername = document.createElement('p');
-  const cardLocation = document.createElement('p');
-  const cardProfile = document.createElement('p');
-  const cardProfileAddress = document.createElement('a');
-  const cardFollowers = document.createElement('p');
-  const cardFollowing = document.createElement('p');
-  const cardBio = document.createElement('p');
+function cardMaker({ name, avatar_url, login, location, gitHub, url, followers, following, bio, html_url }) {
 
-  // time to give all the JSS variables content
-  cardImage.src = avatar_url;
-  cardName.textContent = name;
-  cardUsername.textContent = login;
-  cardLocation.textContent = location;
-  cardProfile.textContent = bio;
-  cardProfileAddress.textContent = html_url;
-  cardProfileAddress.href = html_url;
-  cardFollowers.textContent = 'Followers: ${followers}';
-  cardFollowing.textContent = 'Following: ${following}';
-  cardBio.textContent = 'Bio: ${bio}';
+  //1st create JSS variables to correspond with the HTML structure
+  const userName = document.createElement('div')
+  const profilePic = document.createElement('img')
+  const information = document.createElement('div')
+  const realName = document.createElement('h3')
+  const userLogin = document.createElement('p')
+  const userLocation = document.createElement('p')
+  const profile = document.createElement('p')
+  const address = document.createElement('a')
+  const userFollowers = document.createElement('p')
+  const userFollowing = document.createElement('p')
+  const userBio = document.createElement('p')
 
-  //then we arrange elements in proper order
-  cardCard.appendChild(cardImage)
-  cardCard.appendChild(cardInfoDiv)
-  cardInfoDiv.appendChild(cardName)
-  cardInfoDiv.appendChild(cardUsername)
-  cardInfoDiv.appendChild(cardLocation)
-  cardInfoDiv.appendChild(cardProfile)
-  cardInfoDiv.appendChild(cardFollowers)
-  cardInfoDiv.appendChild(cardFollowing)
-  cardInfoDiv.appendChild(cardBio)
-  cardProfile.appendChild(cardProfileAddress)
+  // provide structure to the variables, using HTML structure as guidance
+  userName.appendChild(profilePic)
+  userName.appendChild(information)
+  information.appendChild(userLogin)
+  information.appendChild(realName)
+  information.appendChild(userLocation)
+  information.appendChild(profile)
+  information.appendChild(userFollowers)
+  information.appendChild(userFollowing)
+  information.appendChild(userBio)
+  profile.appendChild(address)
 
-  // assign classes
-  cardCard.classList.add('card');
-  cardInfoDiv.classList.add('card-info');
-  cardName.classList.add('name');
-  cardUsername.classList.add('username');
+  // apply classes to the JSS variables
+  userName.classList.add('card')
+  information.classList.add('card-info')
+  realName.classList.add('name')
+  userLogin.classList.add('username')
 
-//return the panel
-return cardCard;
+  // add content to JSS variables
+  profilePic.setAttribute('src', avatar_url)
+  profilePic.setAttribute('alt', name)
+  realName.textContent = name
+  userLogin.textContent = login
+  userLocation.textContent = location
+  profile.textContent = gitHub
+  address.textContent = url
+  userFollowers.textContent = followers
+  userFollowing.textContent = following
+  userBio.textContent = bio
+  address.textContent = html_url
 
-
-
-
+  return userName
 }
+
+// followersArray.forEach(({  name, login, location, gitHub, url, followers, following, bio, html_url}) => {
+//   const userName = cardMaker(  name, login, location, gitHub, url, followers, following, bio, html_url)
+//   cardCreator.appendChild(userName)
+// })
+
+
+
+
